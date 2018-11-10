c1e8ded0605aee609570e200522c4150dd6576cd
Victor <annimon119@gmail.com>
2018-11-10 18:11:15 +0200

    Rename UpdateRegister with UpdateHandler

--- module/src/main/java/com/annimon/tgbotsmodule/analytics/UpdateHandler.java
+++ module/src/main/java/com/annimon/tgbotsmodule/analytics/UpdateHandler.java
@@ -3,13 +3,13 @@ package com.annimon.tgbotsmodule.analytics;
 import org.jetbrains.annotations.NotNull;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public interface UpdateRegister {
+public interface UpdateHandler {
 
     /**
-     * Registers new Update
+     * Handles new Update
      *
      * @param update  bot update
-     * @return {@code true} if update was register, {@code false} otherwise
+     * @return {@code true} if update was handled, {@code false} otherwise
      */
-    boolean registerUpdate(@NotNull Update update);
+    boolean handleUpdate(@NotNull Update update);
 }
