06cfdbb5aec6590289d4f685fbac52ad03542c97
Fyodor Doletov <doletov.fyodor@yandex.ru>
2021-03-10 11:58:16 +0300

    Add CallbackQueryContext Add argumentsLimit in MessageContext Add deleteMessage in MessageContext

---
+++ module/src/main/java/com/annimon/tgbotsmodule/commands/context/CallbackQueryContext.java
@@ -0,0 +1,126 @@
+package com.annimon.tgbotsmodule.commands.context;
+
+import com.annimon.tgbotsmodule.api.methods.Methods;
+import com.annimon.tgbotsmodule.api.methods.answerqueries.AnswerCallbackQueryMethod;
+import com.annimon.tgbotsmodule.api.methods.updatingmessages.EditMessageTextMethod;
+import com.annimon.tgbotsmodule.services.CommonAbsSender;
+import org.jetbrains.annotations.NotNull;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+
+public class CallbackQueryContext extends Context {
+
+    private String[] arguments;
+    private int argumentsLimit;
+
+    CallbackQueryContext(CommonAbsSender sender, Update update, User user) {
+        super(sender, update, user);
+    }
+
+    public @NotNull String queryId() {
+        return update.getCallbackQuery().getId();
+    }
+
+    public @NotNull CallbackQuery callbackQuery() {
+        return update.getCallbackQuery();
+    }
+
+    public @NotNull Message message() {
+        return update.getCallbackQuery().getMessage();
+    }
+
+    public String data() {
+        return update.getCallbackQuery().getData();
+    }
+
+    public String gameShortName() {
+        return update.getCallbackQuery().getGameShortName();
+    }
+
+    public @NotNull String argument(int index) {
+        return argument(index, "");
+    }
+
+    public @NotNull String argument(int index, @NotNull String defaultValue) {
+        lazyCreateArguments();
+        if (index >= 0 && index < arguments.length) {
+            return arguments[index];
+        }
+        return defaultValue;
+    }
+
+    public @NotNull String[] arguments() {
+        lazyCreateArguments();
+        return arguments;
+    }
+
+    public int argumentsLength() {
+        return arguments().length;
+    }
+
+    public int argumentsLimit() {
+        return argumentsLimit;
+    }
+
+    private void createArguments() {
+        arguments = update.getCallbackQuery().getData().split("\\s+", argumentsLimit);
+    }
+
+    private void lazyCreateArguments() {
+        if (arguments == null) {
+            createArguments();
+        }
+    }
+
+    public void setArgumentsLimit(int argumentsLimit) {
+        if (argumentsLimit == this.argumentsLimit)
+            return;
+
+        this.argumentsLimit = argumentsLimit;
+        createArguments();
+    }
+
+    public @NotNull AnswerCallbackQueryMethod answer(String text, boolean showAsAlert) {
+        return Methods.answerCallbackQuery(queryId())
+                .setText(text)
+                .setShowAlert(showAsAlert);
+    }
+
+    public @NotNull AnswerCallbackQueryMethod answer(String text) {
+        return answer(text, false);
+    }
+
+    public @NotNull AnswerCallbackQueryMethod answerAsAlert(String text) {
+        return answer(text, true);
+    }
+
+    public @NotNull AnswerCallbackQueryMethod answerWithUrl(String url) {
+        return Methods.answerCallbackQuery(queryId())
+                .setUrl(url);
+    }
+
+    public @NotNull EditMessageTextMethod editMessage(String text, InlineKeyboardMarkup markup) {
+        return Methods.editMessageText()
+                .setChatId(message().getChatId())
+                .setMessageId(message().getMessageId())
+                .setText(text)
+                .setReplyMarkup(markup);
+    }
+
+    public @NotNull EditMessageTextMethod editMessage(String text) {
+        return editMessage(text, null);
+    }
+
+    public @NotNull EditMessageTextMethod removeMarkup() {
+        return Methods.editMessageText()
+                .setChatId(message().getChatId())
+                .setMessageId(message().getMessageId())
+                .setText(message().getText())
+                .setEntities(message().getEntities())
+                .setReplyMarkup(null);
+    }
+
+}
