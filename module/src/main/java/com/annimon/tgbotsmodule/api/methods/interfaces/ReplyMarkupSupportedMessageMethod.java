8e5f9e40479fd8a9ce72b03636525fd9430b6284
aNNiMON <annimon119@gmail.com>
2023-02-07 22:03:49 +0200

    More helper methods in ReplyMarkupSupportedMessageMethod

--- module/src/main/java/com/annimon/tgbotsmodule/api/methods/interfaces/ReplyMarkupSupportedMessageMethod.java
+++ module/src/main/java/com/annimon/tgbotsmodule/api/methods/interfaces/ReplyMarkupSupportedMessageMethod.java
@@ -1,7 +1,12 @@
 package com.annimon.tgbotsmodule.api.methods.interfaces;
 
 import java.io.Serializable;
+import java.util.List;
+
+import java.util.stream.Collectors;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 public interface ReplyMarkupSupportedMessageMethod<M extends Method, T extends Serializable>
         extends SendableMessageMethod<M, T> {
@@ -9,4 +14,29 @@ public interface ReplyMarkupSupportedMessageMethod<M extends Method, T extends S
     ReplyKeyboard getReplyMarkup();
 
     M setReplyMarkup(ReplyKeyboard replyMarkup);
+
+    default M setInlineKeyboard(List<List<InlineKeyboardButton>> keyboard) {
+        return setReplyMarkup(new InlineKeyboardMarkup(keyboard));
+    }
+
+    default M setInlineKeyboard(InlineKeyboardButton button) {
+        return setSingleRowInlineKeyboard(button);
+    }
+
+    default M setSingleRowInlineKeyboard(List<InlineKeyboardButton> row) {
+        return setInlineKeyboard(List.of(row));
+    }
+
+    default M setSingleRowInlineKeyboard(InlineKeyboardButton... buttons) {
+        return setSingleRowInlineKeyboard(List.of(buttons));
+    }
+
+    default M setSingleColumnInlineKeyboard(List<InlineKeyboardButton> column) {
+        return setInlineKeyboard(column.stream().map(List::of).collect(Collectors.toList()));
+    }
+
+    default M setSingleColumnInlineKeyboard(InlineKeyboardButton... buttons) {
+        return setSingleColumnInlineKeyboard(List.of(buttons));
+    }
+
 }
