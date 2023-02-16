e76cf4cec886188f83a8d0917044ca4441e4e123
Fyodor Doletov <doletov.fyodor@yandex.ru>
2023-02-16 18:11:17 +0300

    Remove circular dependency between BotHandler and CommandRegistry (#12)
    Remove circular dependency between BotHandler and CommandRegistry

--- module/src/main/java/com/annimon/tgbotsmodule/analytics/UpdateHandler.java
+++ module/src/main/java/com/annimon/tgbotsmodule/analytics/UpdateHandler.java
@@ -1,5 +1,6 @@
 package com.annimon.tgbotsmodule.analytics;
 
+import com.annimon.tgbotsmodule.BotHandler;
 import org.jetbrains.annotations.NotNull;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -11,5 +12,5 @@ public interface UpdateHandler {
      * @param update  bot update
      * @return {@code true} if update was handled, {@code false} otherwise
      */
-    boolean handleUpdate(@NotNull Update update);
+    boolean handleUpdate(@NotNull Update update, BotHandler handler);
 }
